eda1184957cf0ade927f39fef3b7a86fab982b1a
Villem-Oskar Ossip <villem-oskarossip@VillemOskarsMBP.lan>
2022-05-16 15:52:55 +0300

    inheritance

---
+++ coding/basics/inheritance.sol
@@ -0,0 +1,21 @@
+//SPDX-License-Identifier: MIT
+ 
+pragma solidity ^0.8.7;
+
+contract BaseContract{
+    int public x;
+    address public owner;
+
+    constructor(){
+        x = 5;
+        owner = msg.sender;
+    }
+
+    function setX(int _x) public{
+        x = _x;
+    }
+}
+
+contract A is BaseContract{
+    int public y = 3;
+}
