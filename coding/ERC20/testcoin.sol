8af6ed11951b0c403d7286f68777b419b6ef80a8
Villem-Oskar Ossip <villem-oskarossip@VillemOskarsMBP.lan>
2022-05-16 22:20:15 +0300

    erc20 testcoin

--- coding/ERC20/testcoin.sol
+++ coding/ERC20/testcoin.sol
@@ -1,6 +1,10 @@
 //SPDX-License-Identifier: MIT
 
-//https://eips.ethereum.org/EIPS/eip-20
+// ----------------------------------------------------------------------------
+// EIP-20: ERC-20 Token Standard
+// https://eips.ethereum.org/EIPS/eip-20
+// -----------------------------------------
+
  
 pragma solidity ^0.8.7;
 
@@ -9,23 +13,26 @@ interface ERC20Interface {
     function balanceOf(address _tokenOwner) external view returns (uint256 balance);
     function transfer(address _to, uint256 _value) external returns (bool success);
 
-    //Commented out functions are not mamandtory for a erc20 token
-    //function transferFrom(address _from, address _to, uint256 _value) external returns (bool success);
-    //function approve(address _spender, uint256 _value) external returns (bool success);
-    //function allowance(address _owner, address _spender) external view returns (uint256 remaining);
+    //Following functions are not mamandtory for a erc20 token
+    function transferFrom(address _from, address _to, uint256 _value) external returns (bool success);
+    function approve(address _spender, uint256 _value) external returns (bool success);
+    function allowance(address _owner, address _spender) external view returns (uint256 remaining);
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    //event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+    //Following function are not mamandtory for a erc20 token
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 }
 
 contract TestCoin is ERC20Interface{
     string public name = "TestCoin";
     string public symbol = "TC";
     uint8 public decimals = 0; //18 is most used value
-    uint public override totalSupply ;
+    uint public override totalSupply;
 
     address public founder;
     mapping(address => uint) public balances;
+
+    mapping(address => mapping(address => uint)) allowed;
 
     constructor(uint _totalSupply){
         totalSupply = _totalSupply;
@@ -43,7 +50,33 @@ contract TestCoin is ERC20Interface{
         balances[to] += tokens;
         balances[msg.sender] -= tokens;
         emit Transfer(msg.sender, to, tokens);
-        
+
+        return true;
+    }
+
+    function allowance(address tokenOwner, address spender) view public override returns (uint256 remaining){
+        return allowed[tokenOwner][spender];
+    }
+
+    //called by token owner to set the allowance - the amount which can be spent by the spender
+    function approve(address spender, uint256 tokens) public override returns (bool success){
+        require(balances[msg.sender] >= tokens);
+        require(tokens > 0);
+        allowed[msg.sender][spender] = tokens;
+
+        emit Approval(msg.sender, spender, tokens);
+        return true;
+    }
+
+    //allows spender to withdraw or transfer from owners account multiple times multiple times.
+    function transferFrom(address from, address to, uint256 tokens) public override returns (bool success){
+        require(allowed[from][msg.sender] >= tokens);
+        require(balances[from] >= tokens);
+        balances[from] -= tokens;
+        allowed[from][msg.sender] -= tokens;
+        balances[to] += tokens;
+
+        emit Transfer(from, to, tokens);
         return true;
     }
 
